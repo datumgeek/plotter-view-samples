fb5c1229b476ae18e80339f6079cf7e5ded4a4d9
Mike Graham <mgraham@technicalmedia.com>
2016-08-21 07:56:01 -0600

    update globe-view to display the globe

--- views/globe-view/globe-view.js
+++ views/globe-view/globe-view.js
@@ -3,6 +3,24 @@ define(["require", "exports"], function (require, exports) {
     var GlobeView = (function () {
         function GlobeView() {
         }
+        GlobeView.prototype.activate = function () {
+            window.require([
+                "esri/Map",
+                "esri/views/SceneView",
+                "dojo/domReady!"
+            ], function (Map, SceneView) {
+                var map = new Map({
+                    basemap: "streets",
+                    ground: "world-elevation"
+                });
+                var view = new SceneView({
+                    container: "viewDiv",
+                    map: map,
+                    scale: 50000000,
+                    center: [-101.17, 21.78]
+                });
+            });
+        };
         return GlobeView;
     }());
     exports.GlobeView = GlobeView;
--- views/globe-view/globe-view.ts
+++ views/globe-view/globe-view.ts
@@ -1,1 +1,24 @@
-export class GlobeView {}
+export class GlobeView {
+    activate() {
+        (<any>window).require([
+            "esri/Map",
+            "esri/views/SceneView",
+            "dojo/domReady!"
+        ], function (Map, SceneView) {
+
+            var map = new Map({
+                basemap: "streets",
+                ground: "world-elevation"
+            });
+
+            var view = new SceneView({
+                container: "viewDiv",
+                map: map,
+                scale: 50000000,
+                center: [-101.17, 21.78]
+            });
+
+        });
+
+    }
+}
