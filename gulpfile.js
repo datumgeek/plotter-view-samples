fbded4094b09c9d1066ad7b13d8b55c6dfb17921
Mike Graham <mgraham@technicalmedia.com>
2016-08-21 03:21:09 -0600

    rough in view samples project

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+var ts = require('gulp-typescript');
+var merge = require('merge2');
+var less = require('gulp-less');
+var path = require('path');
+var cache = require('gulp-cached');
+var requirejsOptimize = require('gulp-requirejs-optimize');
+var rename = require('gulp-rename');
+
+gulp.task('build-less', function () {
+  return gulp.src('views/**/*.less')
+    .pipe(cache('less'))
+    .pipe(less())
+    .pipe(gulp.dest('views'));
+});
+
+// gulp.task('build-bundle', ['build-less', 'build-ts'], function() {
+//   return gulp.src('src/app.js')
+//     .pipe(requirejsOptimize({
+//       optimize: 'none',
+//       name: 'pp-manifest',
+//       excludeShallow: ['text'],
+//       paths: {
+//         "aurelia-framework": "empty:",
+//         "aurelia-fetch-client": "empty:",
+//         "aurelia-router": "empty:",
+//         "pplatform": "../src",
+//         "text": "../node_modules/text/text"
+//       }
+//     }))
+//     .pipe(rename("plotter-platform-bundle.js"))
+//     .pipe(gulp.dest('scripts'));
+// });
+
+var tsProject = ts.createProject({
+      typescript: require('typescript'),
+      declaration: false,
+      noExternalResolve: true,
+      target: "es5",
+      module: "amd",
+      emitDecoratorMetadata: true,
+      "experimentalDecorators": true
+    });
+
+gulp.task('build-ts', function () {
+  var tsResult = gulp.src(myPaths.ts,
+    { base: "./" })
+    .pipe(cache('ts'))
+    .pipe(ts(tsProject));
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest('./')),
+    tsResult.js.pipe(gulp.dest('./'))
+  ]);
+});
+
+gulp.task('build', ['build-less', 'build-ts']);
+
+var myPaths = {
+  ts: ['views/**/*.ts', 'typings/**/*.d.ts'],
+  js: "views/**/*.js",
+  html: "views/**/*.html",
+  css: "views/**/*.css",
+  less: "views/**/*.less"
+}
+
+gulp.task('serve', ['build'], function (done) {
+  browserSync({
+    open: false,
+    port: 9000,
+    server: {
+      baseDir: ['.'],
+      middleware: function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+      }
+    }
+  }, done);
+});
+
+function reportChange(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+}
+
+gulp.task('watch', ['serve'], function () {
+  gulp.watch(myPaths.ts, ['build-ts']).on('change', reportChange);
+  gulp.watch(myPaths.less, ['build-less']).on('change', reportChange);
+  gulp.watch(myPaths.js, [browserSync.reload]).on('change', reportChange);
+  gulp.watch(myPaths.html, [browserSync.reload]).on('change', reportChange);
+  gulp.watch(myPaths.css, [browserSync.reload]).on('change', reportChange);
+});
---
+++ typings/pplatform/app/platform/state/state-repository.d.ts
@@ -0,0 +1,20 @@
+import { PakDirectory } from '../pak/pak-directory';
+import { StateSession } from './state-session';
+import { StateDirectory } from './state-directory';
+export interface StateRepository {
+    locked: boolean;
+    uniqueId: string;
+    stateRepositoryType: StateRepositoryType;
+    stateDirectory: StateDirectory;
+    getPakDirectory(): Promise<PakDirectory>;
+    getStateSession(sessionId: string): Promise<StateSession>;
+    getSessionList(): Promise<string[]>;
+    toJSON(): StateRepositoryJSON;
+}
+export interface StateRepositoryJSON {
+    locked: boolean;
+    uniqueId: string;
+    stateRepositoryType: StateRepositoryType;
+    path: string;
+}
+export declare type StateRepositoryType = 'LocalStorage' | 'Service' | 'GitHubGist' | 'File';
